40a97ae0899e0e2b19cdf98dc2f39b5d0f86f954
jonoAvila2123@gmail.com <jonoAvila2123@gmail.com@830d5c70-a541-c734-0a39-5e7b044940b7>
2012-05-03 20:27:11 +0000

    Artistas individaules

---
+++ ReconocimientoLetras/src/reconocimientoletras/Dibujo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reconocimientoletras;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author johnny
+ */
+public class Dibujo {
+    private int x;
+    private int y;
+    private Color color = new Color(0,0,0);
+
+    public Dibujo() {
+    }
+    
+    public void obtenerCordenadas(MouseEvent evt)
+    {
+        if(!evt.isMetaDown())
+        {
+            
+        }
+    }
+}
